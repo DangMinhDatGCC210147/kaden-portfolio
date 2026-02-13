e0d301c1c34c682b5dad197db2e22b84fcedee89
minhdatdustin <minhdatnvh1510@gmail.com>
2026-02-13 13:39:03 +0700

    fix bugs 3

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
-import { LegendProps } from "recharts"
+// import { LegendProps } from "recharts"
 
 import { cn } from "./utils"
 
